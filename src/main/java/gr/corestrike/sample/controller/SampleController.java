9a60795d2ee57469ce0d24fb2e0e9f7f8f5c3443
Phillip Webb <pwebb@gopivotal.com>
2014-06-27 11:01:08 -0700

    Extract ApplicationServletInitializer
    Extract ApplicationServletInitializer from Application and also add the
    @Configuration annotation to Application.

--- src/main/java/gr/corestrike/sample/controller/SampleController.java
+++ src/main/java/gr/corestrike/sample/controller/SampleController.java
@@ -1,12 +1,11 @@
 package gr.corestrike.sample.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/sample")
@@ -24,8 +23,6 @@ public class SampleController {
     @RequestMapping("/angular/")
     public String index2(Map<String, Object> model) {
         Date date = new Date();
-        SimpleDateFormat format = new SimpleDateFormat();
-        String str = "Reached server controller: " + format.format(date);
         model.put("sample", "Reached server controller: " +date.getTime());
         return "sample/dist/index";
     }
